87d21bbabc5051627f890d348b8f16cee437b3ec
dhightnm <dhightnm@gmail.com>
2021-06-29 18:45:02 -0700

    Loaded deployed DB into creds

--- server/connect.js
+++ server/connect.js
@@ -2,8 +2,8 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 // Option 2: Passing parameters separately (other dialects)
 
-const sequelize = new Sequelize('reviewTest', 'devin', process.env.DB_PASSWORD, {
-  host: 'localhost',
+const sequelize = new Sequelize('reviews', 'postgres', 'bored1176carpet', {
+  host: 'ec2-3-139-100-1.us-east-2.compute.amazonaws.com',
   dialect: 'postgres',
 });
 
